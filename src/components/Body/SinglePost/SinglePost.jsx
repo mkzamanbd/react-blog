b622b601831220994404999bf37a68de954211e0
kamruzzaman <zaman7u@gmail.com>
2021-10-10 17:18:30 +0600

    Update SinglePost.jsx

--- src/components/Body/SinglePost/SinglePost.jsx
+++ src/components/Body/SinglePost/SinglePost.jsx
@@ -7,7 +7,7 @@ function SinglePost() {
     const [isLoded, setIsLoded] = useState({ isLoded: false });
 
     useEffect(() => {
-        fetch("http://laravel-practice.test/api/v1/post?slug=" + params.slug)
+        fetch("https://api.kzaman.me/api/v1/post?slug=" + params.slug)
             .then((res) => {
                 return res.json();
             })
